7980833010f39e835447ac1edeede90bb25fa10e
Businge Brian <busingebrian@gmail.com>
2023-11-01 18:26:54 +0300

    added ui for result activity

---
+++ app/src/main/java/com/example/quiz_app/ui/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quiz_app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quiz_app.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
